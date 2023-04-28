c84df3a0bc49854bff6b77d93b4edee9cf98a376
Muhammad <muhammad.martazanov.13@mail.ru>
2023-04-28 17:20:17 +0300

    first project

---
+++ src/components/Categories.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+const Categories = ({ items, onClickItem }) => {
+    const [activeItem, setActiveItem] = React.useState(null)
+
+    const onSelectItem = (index) => {
+        setActiveItem(index)
+    }
+
+    return (
+        <div className="categories">
+            <ul>
+                <li className={activeItem === null ? 'active' : ''}>Все</li>
+                {items ?
+                    items.map((item, index) => (
+                        <li className={activeItem === index ? "active" : ''} onClick={() => onSelectItem(index)} key={`${item}_${index}`}>{item}</li>
+               )) : undefined}
+            </ul>
+        </div>
+    );
+}
+
+export default Categories; 
